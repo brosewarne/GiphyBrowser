59896f1968d95660ce691c85b367a423d1478386
brosewarne <ben.rosewarne@gmail.com>
2024-09-15 20:37:28 +1000

    oops, removed leftover from testing

--- src/components/gifGrid/gifGrid.tsx
+++ src/components/gifGrid/gifGrid.tsx
@@ -16,13 +16,17 @@ export const GifGrid = memo(function GifGrid({
   gifData: GiphyGif[];
 }) {
   const theme = useTheme();
-  const rows = useMemo(() => gifData.map((gif: GiphyGif, index: number) => {
-    return (
-      <Grid size={4} columnGap={2} key={`gifGrid_${index}`}>
-        <GifTile gifData={gif} key={gif.id}></GifTile>
-      </Grid>
-    );
-  }), [gifData]);
+  const rows = useMemo(
+    () =>
+      gifData.map((gif: GiphyGif, index: number) => {
+        return (
+          <Grid size={4} columnGap={2} key={`gifGrid_${index}`}>
+            <GifTile gifData={gif} key={gif.id}></GifTile>
+          </Grid>
+        );
+      }),
+    [gifData],
+  );
 
   return (
     <Box
@@ -31,7 +35,6 @@ export const GifGrid = memo(function GifGrid({
       marginBottom={theme.spacing(1)}
       data-testid="gif-grid"
     >
-      <h1>here</h1>
       <Grid container rowSpacing={3} columnSpacing={2}>
         {rows}
       </Grid>
